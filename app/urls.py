fcfdfe51cf632bc0f949ce6d260d0f997d4bd59d
KTamara0 <tamara.kaselj00@gmail.com>
2025-06-27 19:58:07 +0200

    Link for superuser

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from .views import MyPasswordChangeView, MyPasswordChangeDoneView
 
 app_name = "app"
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('login/', views.login_view, name='login'),
     path('register/', views.register, name='register'),
